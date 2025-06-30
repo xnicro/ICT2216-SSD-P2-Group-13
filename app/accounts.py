58359565c583d99178f6b36368c8bc13d6d0130d
y2git <quah.y2@gmail.com>
2025-06-30 22:08:15 +0800

    a

--- app/accounts.py
+++ app/accounts.py
@@ -1,8 +1,10 @@
 import mysql.connector
 import os
 from flask import Flask, Blueprint, current_app, request, session, redirect, url_for
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
 
-bp = Blueprint('register_user', __name__)
+bp = Blueprint('accounts', __name__)
 
 def get_db_connection():
     cfg = current_app.config
@@ -13,7 +15,9 @@ def get_db_connection():
         database=cfg['MYSQL_DB'],
     )
 
-    
+
+
+# POST routes =============================================    
 @bp.route("/register", methods=["POST"])
 def register_user():
     if request.method == 'POST':
@@ -28,12 +32,10 @@ def register_user():
         try:
             conn = get_db_connection()
             cursor = conn.cursor()
-            cursor.execute('SELECT * FROM test;')
-            conn.commit()
 
             # Check if username or email already exists
-            check_query = "SELECT * FROM users WHERE username = %s"
-            cursor.execute(check_query, (username))
+            check_query = "SELECT * FROM users WHERE username = %s OR email = %s"
+            cursor.execute(check_query, (username, email))
             if cursor.fetchone():
                 return "Username or email already exists", 400
             else:
@@ -41,13 +43,10 @@ def register_user():
                 insert_query = """INSERT INTO users (username, email, pwd) VALUES (%s, %s, %s)"""
                 cursor.execute(insert_query, (username, email, password))
                 conn.commit()
+                
             cursor.close()
             conn.close()
-            return redirect(url_for('success'))
+            return "Registration Success"
         except Exception as e:
             return f'MySQL connection error: {str(e)}'
 
-
-@bp.route('/success')
-def success():
-    return "Registration successful!"
