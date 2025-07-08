88739f739e6afbdcde5316ef9cf383968ea49d69
xnicro <xnicro@gmail.com>
2025-07-08 18:55:12 +0800

    updated env files, sorry yall

--- setup_env.bat
+++ setup_env.bat
@@ -16,6 +16,11 @@ echo SMTP_PORT=587
 echo SENDER_EMAIL=sitsecure.notifications@gmail.com
 echo SENDER_PASSWORD=wurhnkuxldbfnokf
 echo APP_NAME=SITSecure
+echo GRAYLOG_MONGO_PASSWORD=graylog123
+echo GRAYLOG_PASSWORD_SECRET=someveryverylongpasswordpepper
+echo GRAYLOG_ROOT_PASSWORD_SHA2=8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918
+echo GRAYLOG_HOST=graylog
+echo GRAYLOG_PORT=12201
 ) > .env
 
 echo.
