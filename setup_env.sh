d6f00c6ac895bd942926c7f7879e0e25f9522daa
xnicro <xnicro@gmail.com>
2025-07-08 18:57:16 +0800

    updated env files, sorry yall

--- setup_env.sh
+++ setup_env.sh
@@ -15,6 +15,12 @@ SMTP_PORT=587
 SENDER_EMAIL=sitsecure.notifications@gmail.com
 SENDER_PASSWORD=wurhnkuxldbfnokf
 APP_NAME=SITSecure
+
+GRAYLOG_MONGO_PASSWORD=graylog123
+GRAYLOG_PASSWORD_SECRET=someveryverylongpasswordpepper
+GRAYLOG_ROOT_PASSWORD_SHA2=8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918
+GRAYLOG_HOST=graylog
+GRAYLOG_PORT=12201
 EOF
 
 echo ".env file created!"
